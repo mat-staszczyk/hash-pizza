5e2b40a443223879c3cbabbb487df1dcc6b8850f
sleaz0id <mat.staszczyk@gmail.com>
2017-02-11 19:13:11 +0100

    Set pointer after taking a slice and preparation for complex pizza cuttin'

--- main.rb
+++ main.rb
@@ -22,20 +22,31 @@ class Pizza
 end
 
 class PizzaCutter
+  attr_reader :slices, :pizza, :pizza_left, :pointer
+
   def initialize(pizza)
     @pizza = pizza.pizza_arr
+    @tomatoes = pizza.tomatoes_count
+    @mushrooms = pizza.mushrooms_count
     @slices = []
     @settings = pizza.settings
     @pointer = [0, 0] #start in the bottom left corner of the pizza, so that it resembles the coordinate system
+    @pizza_left = @settings[:rows] * @settings[:columns]
     # testing
     pizza.verify_size
+  end
+
+  def cut_pizza
+    until @pizza_left.zero? || @mushrooms < @settings[:min_ings] || @tomatoes < @settings[:min_ings] do
+      # todo
+    end 
   end
 
   def take_slice
     slice = find_slice(@settings[:max_cells])
     cut_slice(slice)
-    add_slice(slice)
-    # change the pointer
+    set_pointer
+    serve_slice(slice)
   end
 
   private
@@ -50,10 +61,16 @@ class PizzaCutter
     def cut_slice(slice)
       #slice represented as an array [x0, x1, y0, y1]
       x0, x1, y0, y1 = slice
-      (x0..x1).each {|x| @pizza[x][y0..y1] = Array.new(y1 - y0 + 1)}
+      (x0..x1).each do |x| 
+        rows_to_cut = y1 - y0 + 1
+        @pizza[x][y0..y1] = Array.new(rows_to_cut)
+        @pizza_left -= rows_to_cut 
+        @tomatoes = @pizza.flatten.count('T')
+        @mushrooms = @pizza.flatten.count('M')
+      end
     end
 
-    def add_slice(slice)
+    def serve_slice(slice)
       @slices << slice
     end
 
@@ -68,7 +85,7 @@ class PizzaCutter
         ing_count[0] += subsection.count('T')
         ing_count[1] += subsection.count('M')
       end
-     ing_count.min >= @settings[:min_ings] and (x1 - x0) * (y1 - y0) <= @settings[:max_cells]
+      ing_count.min >= @settings[:min_ings] and (x1 - x0) * (y1 - y0) <= @settings[:max_cells]
     end
 
     def possible_rectangles(area)
@@ -76,6 +93,14 @@ class PizzaCutter
       ary.map! {|x| [x, ary.pop]}
       ary += ary.map(&:reverse)
       ary.sort_by {|rectangle| rectangle.reduce(:+)}
+    end
+
+    def set_pointer
+      @pizza.each_with_index do |row, r|
+        c = row.find_index { |x| x }
+        @pointer = [r,c]
+        return @pointer if c
+      end
     end
 end
 
