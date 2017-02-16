e58e9c5363a790bee53a0cf593321e950f7c859e
sleaz0id <mat.staszczyk@gmail.com>
2017-02-16 22:21:32 +0100

    Looking for biggest slice with one ingridient

--- main.rb
+++ main.rb
@@ -45,29 +45,28 @@ class PizzaCutter
 
   def take_slice
     slice = find_slice(@settings[:max_cells])
-    cut_slice(slice)
+    cut_slice(slice) if slice
+    serve_slice(slice) if slice
     set_pointer
-    serve_slice(slice)
   end
 
-  #private
+  private
 
     def find_slice(area)
       # find the first possible slice
-      rects = possible_rectangles(area)
-      #first = ary.find {|rect| is_valid?([@pointer[0], @pointer[0] + rect[0] - 1, @pointer[1], @pointer[1] + rect[1] - 1])}
-      #first = [@pointer[0], @pointer[0] + first[0] - 1, @pointer[1], @pointer[1] + first[1] - 1]
+      rects = all_possible_rectangles(area)
       pointer_row = @pointer[0]
       pointer_col = @pointer[1]
       rects.map! { |r| [pointer_row, pointer_col, 
                         pointer_row + r[0] - 1, 
                         pointer_col + r[1] - 1] }
+      p rects
       rects.detect { |r| is_valid?(r) }
     end
 
     def cut_slice(slice)
       #slice represented as an array [x0, x1, y0, y1]
-      x0, x1, y0, y1 = slice
+      x0, y0, x1, y1 = slice
       (x0..x1).each do |x| 
         rows_to_cut = y1 - y0 + 1
         @pizza[x][y0..y1] = Array.new(rows_to_cut)
@@ -93,7 +92,7 @@ class PizzaCutter
         ing_count[0] += subsection.count('T')
         ing_count[1] += subsection.count('M')
       end
-      ing_count.min >= @settings[:min_ings] and (x1 - x0) * (y1 - y0) <= @settings[:max_cells]
+      ing_count.min == @settings[:min_ings] and (x1 - x0) * (y1 - y0) <= @settings[:max_cells]
     end
 
     def in_range?(slice)
@@ -108,7 +107,16 @@ class PizzaCutter
       ary.sort_by {|rectangle| rectangle.reduce(:+)}
     end
 
+    def all_possible_rectangles(area)
+      ary = []
+      (2..area).each do |i|
+        ary += possible_rectangles(i)
+      end
+      ary.uniq.sort.reverse
+    end
+
     def set_pointer
+      # Todo: find the next available pointer greater than current one
       @pizza.each_with_index do |row, r|
         c = row.find_index { |x| x }
         @pointer = [r,c]
