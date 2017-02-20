d1984bf90eb819bf68163e2fb7c6a87d91ac95ad
jerryskye <jarek@siedlarz.com>
2017-02-20 11:40:07 +0100

    yaml dumping in case of pkill

--- exec.rb
+++ exec.rb
@@ -1,9 +1,16 @@
+require 'yaml'
 require './main.rb'
+
 filepath = ARGV.first
-file = File.basename filepath
+file = File.basename filepath, ".*"
 
 pizza = Pizza.new(filepath)
 cutter = PizzaCutter.new(pizza)
+begin
 cutter.cut_pizza
+rescue SignalException => e
+	File.write("output/%s.yml" % file, YAML.dump(cutter))
+	puts e.class
+end
 
-cutter.save_result("output/%s" % file.gsub(/\..+/, '.txt'))
+cutter.save_result("output/%s.txt" % file)
